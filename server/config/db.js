08d401e2fef0dbe5b57436c740fbe1728463fbdf
Ajstyles <janup@gmail.com>
2025-10-22 15:52:24 +0530

    some changes to DB files

--- server/config/db.js
+++ server/config/db.js
@@ -1,11 +1,9 @@
 import mongoose from "mongoose"
 // process.env.MONGO_URI |   'mongodb://localhost:27017/'
-const connectDB = async () => {
+const connectDB =  () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+      mongoose.connect(process.env.MONGO_URI, {
       dbName:"kanpurClassic",
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
     })
     console.log('MongoDB connected')
   } catch (error) {
--- server/server.js
+++ server/server.js
@@ -4,11 +4,9 @@ import connectDB from './config/db.js'
 
 const PORT = process.env.PORT || 8000
 
-connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-  })
-}).catch((err) => {
-  console.error('Failed to connect to MongoDB:', err)
-  process.exit(1)
+connectDB();
+
+app.listen(PORT,()=>{
+  console.log("server is running good");
+  
 })
